936f6ce3f9c36f099b8673b91155b69afeae0f57
Jacob Rivera <edjacob25@gmail.com>
2019-08-07 00:32:40 -0500

    Remove jquery

--- js/js.js
+++ js/js.js
@@ -1,68 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
+                // Get the target from the "data-target" attribute
+                const target = $el.dataset.target;
+                const $target = document.getElementById(target);
 
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
 
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+            });
+        });
+    }
 
-      });
-    });
-  }
+    document.querySelectorAll('.tabs ul li')
+        .forEach(a => a.addEventListener("click", (event) => {
+            const item = event.currentTarget;
+            const name = item.getAttribute("data-name");
+            hideAll();
+            item.classList.add("is-active");
+            document.querySelector("#" + name).classList.remove("is-hidden");
+        }));
 });
 
-var hideAll = function() {
-    $(".tabs ul li").removeClass("is-active");
-    $("#programming").addClass("is-hidden");
-    $("#languages").addClass("is-hidden");
-    $("#technologies").addClass("is-hidden");
-    $("#certifications").addClass("is-hidden");
+const hideAll = function () {
+    document.querySelectorAll('.tabs ul li')
+        .forEach(a => a.classList.remove("is-active"));
+    document.querySelector('#programming').classList.add("is-hidden");
+    document.querySelector('#languages').classList.add("is-hidden");
+    document.querySelector('#technologies').classList.add("is-hidden");
+    document.querySelector('#certifications').classList.add("is-hidden");
+};
+
+function animateCss(element, animationName) {
+    const node = document.querySelector(element);
+    node.classList.add('animated', animationName);
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName);
+        node.removeEventListener('animationend', handleAnimationEnd);
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
 }
-
-$(document).ready(function () {
-    $(".tabs ul li").click(function() {
-        var name = $(this).data("name");
-        hideAll();
-        $(this).addClass("is-active");
-        $("#" + name).removeClass("is-hidden");
-    });
-});
-
-$.fn.extend({
-  animateCss: function(animationName, callback) {
-    var animationEnd = (function(el) {
-      var animations = {
-        animation: 'animationend',
-        OAnimation: 'oAnimationEnd',
-        MozAnimation: 'mozAnimationEnd',
-        WebkitAnimation: 'webkitAnimationEnd',
-      };
-
-      for (var t in animations) {
-        if (el.style[t] !== undefined) {
-          return animations[t];
-        }
-      }
-    })(document.createElement('div'));
-
-    this.addClass('animated ' + animationName).one(animationEnd, function() {
-      $(this).removeClass('animated ' + animationName);
-
-      if (typeof callback === 'function') callback();
-    });
-
-    return this;
-  },
-});
